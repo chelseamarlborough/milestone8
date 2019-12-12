74831458e82837c2ad67ebc21ddc704b7aecb2cf
Chelsea Marlborough <chelseamarlborough@college.harvard.edu>
2019-12-12 14:57:22 -0500

    comments fixed

--- milestone8/app.R
+++ milestone8/app.R
@@ -128,14 +128,15 @@ ui <- fluidPage(
       tabsetPanel(
         tabPanel(
           "Overall Distribution",
-          h3("How often did rap appear in the Top 100 Chart?"),
+          h2("How often did rap appear in the Top 100 Chart?"),
           mainPanel(
             
             #Again, I would like to make the background black here and writing
             #white, along with the rest of the project.
             
             plotOutput("piechart")
-          )
+          ),
+          h3("The rap genre appeard thirty-six times on the Top 100 chart. This is likely due to Hip Hop's ", a("heavy influence on 21st century music", href= "https://www.britannica.com/art/hip-hop/Hip-hop-in-the-21st-century"), " .")
         ),
         tabPanel(
           "Audio Features",
@@ -206,19 +207,19 @@ ui <- fluidPage(
       )
     ),
     tabPanel(
-      "Spotify Definitions",
+      "Audio Feature Definitions",
       mainPanel(
-        h3("All definitions are curtosey of Spotify for Developers."),
-        h5("Acousticess: A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic."), 
-        h5("Dancebility: Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable."), 
-        h5("Duration: The duration of the track in milliseconds."), 
-        h5("Energy: Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy."), 
-        h5("Instrumentalness: Predicts whether a track contains no vocals. “Ooh” and “aah” sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly “vocal”. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0."),
-        h5("Liveliness: Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live."),
-        h5("Loudness: The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typical range between -60 and 0 db."),
-        h5("Speechiness: 	Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks."),
-        h5("Tempo: 	The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration."),
-        h5("Valence: 	A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).")
+        h5(strong("Acousticess:"), "A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic."), 
+        h5(strong("Dancebility:"), "Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable."), 
+        h5(strong("Duration:"), "The duration of the track in milliseconds."), 
+        h5(strong("Energy:"), "Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy."), 
+        h5(strong("Instrumentalness:"), "Predicts whether a track contains no vocals. “Ooh” and “aah” sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly “vocal”. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0."),
+        h5(strong("Liveliness:"), "Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live."),
+        h5(strong("Loudness:"), "The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typical range between -60 and 0 db."),
+        h5(strong("Speechiness:"), "Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks."),
+        h5(strong("Tempo:"), "The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration."),
+        h5(strong("Valence:"),"A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry)."),
+        h6("Definitions are courtesy of ", a("Spotify for Developers", href= "https://developer.spotify.com/documentation/web-api/reference/tracks/get-several-audio-features/"), " .")
       )
     ),
     tabPanel(
